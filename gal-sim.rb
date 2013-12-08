40f1bccc5c35eef7cb789ee356cd147a2e50a30b
Dustin Lang <dstndstn@gmail.com>
2013-12-07 21:27:32 -0500

    add gal-sim: a galaxy simulation toolbox.
    GalSim is a package for simulating astronomical images of galaxies.
    It was developed to support the upcoming "Great3" challenge,
    http://great3challenge.info/, to test different methods for measuring
    weak gravitational lensing, a key way of measuring mass in the
    universe through its bending of light.
    
    Conflicts:
    	gal-sim.rb

--- gal-sim.rb
+++ gal-sim.rb
@@ -15,16 +15,20 @@ class GalSim < Formula
   depends_on 'pyfits' => :python
   depends_on 'numpy' => :python
 
+<<<<<<< HEAD
   def pyver
     IO.popen("python -c 'import sys; print sys.version[:3]'").read.strip
   end
 
+=======
+>>>>>>> 1690d84... add gal-sim: a galaxy simulation toolbox.
   def install
     # This ought to be part of a standard homebrew install;
     # required so that homebrew creates symlinks
     #   lib/pythonX.Y/galsim -> Cellar/gal-sim/0.2/lib/pythonX.Y/galsim
     # rather than
     #   lib/pythonX.Y -> Cellar/gal-sim/0.2/lib/pythonX.Y
+<<<<<<< HEAD
     ohai "Python version is #{pyver}"
     mkdir_p "#{HOMEBREW_PREFIX}/lib/python#{pyver}"
     system "scons"
@@ -42,4 +46,26 @@ class GalSim < Formula
     EOS
   end
 
+=======
+    pyver = ''
+    IO.popen("python -c 'import sys; print sys.version[:3]'") {|pv_io|
+        pyver = pv_io.read.strip
+    }
+    ohai "Python version is *#{pyver}*"
+    mkdir_p "#{HOMEBREW_PREFIX}/lib/python#{pyver}"
+
+    system "scons"
+    system "scons install PREFIX=#{prefix} PYPREFIX=#{lib}/python#{pyver}"
+
+    ohai ""
+    ohai "The GalSim installer may warn you that #{lib}/python isn't in your python search path."
+    ohai "You may want to add all Homebrew python packages to the default paths by running:"
+    ohai "   sudo bash -c 'echo \"/usr/local/lib/python\" >> \\\\"
+    ohai "     /Library/Python/#{pyver}/site-packages/homebrew.pth'"
+    ohai "Which will create the file   /Library/Python/#{pyver}/site-packages/homebrew.pth"
+    ohai "with contents:"
+    ohai "  /usr/local/lib/python#{pyver}"
+    ohai ""
+  end
+>>>>>>> 1690d84... add gal-sim: a galaxy simulation toolbox.
 end
